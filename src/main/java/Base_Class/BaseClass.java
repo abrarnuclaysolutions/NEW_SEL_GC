5f7ffae3a7fe2f887b9bb6f544756ddf10f8d417
abrar <abrar@AbrarHasan>
2024-10-31 02:17:23 +0530

    first commet

---
+++ src/main/java/Base_Class/BaseClass.java
@@ -0,0 +1,13 @@
+package Base_Class;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	//username
+	public String username = "abrark_gc";
+	//password
+	public String pass = "tiger123";	
+	
+}
